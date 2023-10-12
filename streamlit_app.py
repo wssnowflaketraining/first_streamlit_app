a02db64458781ab3fe1e0f65d7e2c74b18429229
wssnowflaketraining <147588832+wssnowflaketraining@users.noreply.github.com>
2023-10-12 14:34:02 +0800

    Update streamlit_app.py
    Adding text entry box and send input to the fruityvice as part of the API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,11 +21,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-#New Section to display fruityvice api response
-streamlit.header('Fruityvice Fruit Advice')
+#New Section to display fruityvice api response, and add text entry box and send input to fruityvice as part of the API call
+streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit choice)
 
 # take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
