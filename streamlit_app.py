9df7954ebb2a31d7816b61642fda21d2d51142ed
wssnowflaketraining <147588832+wssnowflaketraining@users.noreply.github.com>
2023-10-13 15:36:40 +0800

    Update streamlit_app.py
    typo correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error importn URLError
+from urllib.error import URLError
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
