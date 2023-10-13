986ef99e3873a8884328b348365bb131660d531f
wssnowflaketraining <147588832+wssnowflaketraining@users.noreply.github.com>
2023-10-13 15:43:06 +0800

    Update streamlit_app.py
    Nested if else

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,17 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response, and add text entry box and send input to fruityvice as part of the API call
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+  else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice())
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e
+      streamlit.error()
 
 #import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
