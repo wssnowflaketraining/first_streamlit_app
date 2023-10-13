ccdf8cb13b50c9d8e0c4e6a3f442be86e72dc223
wssnowflaketraining <147588832+wssnowflaketraining@users.noreply.github.com>
2023-10-13 15:14:47 +0800

    Update streamlit_app.py
    Allos user to add fruit to list

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,7 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+# Adding a second text box
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
